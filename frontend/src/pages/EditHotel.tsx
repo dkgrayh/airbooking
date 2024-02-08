6dc196bb32595eca1a5da204753011660007359e
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2024-02-08 07:04:58 -0500

    Added test for view details in edit hotel page

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,9 +2,12 @@ import { useMutation, useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import * as apiClient from '../api-client';
 import ManageHotelForm from '../forms/ManageHotelForm/ManageHotelForm';
+import { useAppContext } from '../contexts/AppContext';
 
 const EditHotel = () => {
   const { hotelId } = useParams();
+
+  const { showToast } = useAppContext();
 
   const { data: hotel } = useQuery(
     'fetchMyHotelById',
@@ -13,8 +16,12 @@ const EditHotel = () => {
   );
 
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
-    onSuccess: () => {},
-    onError: () => {},
+    onSuccess: () => {
+      showToast({ message: 'Hotel Saved!' as 'string', type: 'SUCCESS' });
+    },
+    onError: () => {
+      showToast({ message: 'Error Saving Hotel' as 'string', type: 'ERROR' });
+    },
   });
 
   const handleSave = (hotelFormData: FormData) => {
