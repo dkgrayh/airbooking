1a95adc1dc7a1e46b82fea0d0a8a537e2e5f2d23
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2024-02-09 10:06:10 -0500

    Create search bar component and added route to the search page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from './pages/AddHotel';
 import { useAppContext } from './contexts/AppContext';
 import MyHotels from './pages/MyHotels';
 import EditHotel from './pages/EditHotel';
+import Search from './pages/Search';
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -27,14 +28,10 @@ function App() {
             }
           />
           <Route
-            path="/search-page"
-            element={<>Search Page</>}
-          />
-          <Route
-            path="*"
+            path="/search"
             element={
               <Layout>
-                <p>Search Page</p>
+                <Search />
               </Layout>
             }
           />
---
+++ frontend/src/pages/Search.tsx
@@ -0,0 +1,8 @@
+import { useSearchContext } from '../contexts/SearchContext';
+
+const Search = () => {
+  const search = useSearchContext();
+  console.log(search);
+  return <>Search Page</>;
+};
+export default Search;
