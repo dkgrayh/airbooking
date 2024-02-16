ea29f2172209572f3968d660bfa05b7233d09c47
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2024-02-16 09:14:07 -0500

    Added sort price in search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -8,6 +8,7 @@ import Pagination from '../components/Pagination';
 import StarRatingFilter from '../components/StarRatingFilters';
 import HotelTypesFilter from '../components/HotelTypesFilter';
 import FacilitiesFilter from '../components/FacilitiesFilter';
+import PriceFilter from '../components/PriceFilter';
 
 const Search = () => {
   const search = useSearchContext();
@@ -15,6 +16,7 @@ const Search = () => {
   const [selectedStars, setSelectedStars] = useState<string[]>([]);
   const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
   const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
+  const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
 
   const searchParams = {
     destination: search.destination,
@@ -26,6 +28,7 @@ const Search = () => {
     stars: selectedStars,
     types: selectedHotelTypes,
     facilities: selectedFacilities,
+    maxPrice: selectedPrice?.toString(),
   };
 
   const { data: hotelData } = useQuery<HotelSearchResponse>(
@@ -88,6 +91,10 @@ const Search = () => {
           <FacilitiesFilter
             selectedFacilities={selectedFacilities}
             onChange={handleFacilityChange}
+          />
+          <PriceFilter
+            selectedPrice={selectedPrice}
+            onChange={(value?: number) => setSelectedPrice(value)}
           />
         </div>
       </div>
